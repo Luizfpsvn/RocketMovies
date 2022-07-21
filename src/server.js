c22686b47ef0b4cd0771bcd61a3f38aeba3fbacf
Luiz Fernando Pinto de Souza Verly do Nascimento <luizfernando.cg@hotmail.com>
2022-07-21 09:36:14 -0300

    created database.db

--- src/server.js
+++ src/server.js
@@ -1,13 +1,19 @@
 require("express-async-errors");
+
+const database = require("./database/sqlite"); 
+
+const AppError = require("./utils/AppError");
+
 const express = require('express');
 
 const routes = require("./routes");
-const AppError = require("./utils/AppError");
 
 const app = express();
 app.use(express.json());
 
 app.use(routes);
+
+database();
 
 app.use((error, request, response, next) => {
   if(error instanceof AppError) {
@@ -22,7 +28,6 @@ app.use((error, request, response, next) => {
     message: "Internal Server Error"
   });
 });
-
 
 PORT = 3333;
 app.listen(PORT, () => console.log(`The server is running on PORT ${PORT}`));
