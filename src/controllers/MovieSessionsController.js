fd1400fb41f7631bc7c510ba01a72e75e7f17268
Luiz Fernando <luizfernando.cg@hotmail.com>
2022-08-28 08:16:07 -0300

    Create a validation if the password matches with the user password in DB

--- src/controllers/MovieSessionsController.js
+++ src/controllers/MovieSessionsController.js
@@ -1,3 +1,4 @@
+const { compare } = require("bcryptjs");
 const knex = require("../database/knex");
 const AppError = require("../utils/AppError");
 
@@ -8,6 +9,12 @@ class MovieSessionsController {
     const user = await knex("users").where({ email }).first();
 
     if (!user) {
+      throw new AppError("Email and/or Password is wrong!", 401);
+    }
+
+    const passwordMatched = await compare(password, user.password);
+
+    if (!passwordMatched) {
       throw new AppError("Email and/or Password is wrong!", 401);
     }
 
