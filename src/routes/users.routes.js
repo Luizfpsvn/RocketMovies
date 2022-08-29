78e5c3594d9e6c18d3be72bd26c93dcd5ff9ddb4
Luiz Fernando <luizfernando.cg@hotmail.com>
2022-08-29 02:45:00 -0300

    created a patch route in users.routes, so the user now can patch and send the avatar via multipartform file

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,8 +1,13 @@
 const { Router } = require("express");
 
+const multer = require("multer");
+const uploadConfig = require("../configs/upload");
+
 const UsersController = require("../controllers/UsersController");
 
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
+
+const upload = multer(uploadConfig.MULTER);
 
 const usersRoutes = Router();
 
@@ -10,5 +15,15 @@ const usersController = new UsersController();
 
 usersRoutes.post("/", usersController.create);
 usersRoutes.put("/", ensureAuthenticated, usersController.update);
+usersRoutes.patch(
+  "/avatar",
+  ensureAuthenticated,
+  upload.single("avatar"),
+  (request, response) => {
+    console.log(request.file.filename);
+
+    return response.json();
+  }
+);
 
 module.exports = usersRoutes;
